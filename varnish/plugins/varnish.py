c510ba3dd6ac7856399129c77927d438d426f12e
sacreman <sacreman@gmail.com>
2015-11-28 23:39:22 +0000

    adds a pack for varnish

---
+++ varnish/plugins/varnish.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+import time
+
+command = "/usr/bin/varnishstat -1 | awk '{print $1,$2}'"
+
+
+def get_varnish_metrics():
+    try:
+        status = subprocess.check_output(command, shell=True)
+        m = {}
+        for line in status.splitlines():
+            name = line.split()[0].lower().replace('(', '_').replace(')', '_').replace(',,', '_')
+            m[name] = line.split()[1]
+        return m
+
+    except Exception, e:
+        print "Plugin Failed! %s" % e
+        sys.exit(2)
+
+time_between = 5
+
+metrics_before = get_varnish_metrics()
+time.sleep(time_between)
+metrics = get_varnish_metrics()
+
+metric_rates = {}
+for metric, value in metrics.iteritems():
+    if value > metrics_before[metric]:
+        metric_rates[metric + '_per_sec'] = (int(value) - int(metrics_before[metric])) / time_between
+
+metrics.update(metric_rates)
+
+output = "OK | "
+for k, v in metrics.iteritems():
+    output += str(k) + '=' + str(v) + ';;;; '
+
+print output
+sys.exit(0)
