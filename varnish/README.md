d278287c998d545e8f121419ea2be450b1f7433c
sacreman <sacreman@gmail.com>
2016-05-23 13:58:42 +0100

    fix up varnish pack

--- varnish/README.md
+++ varnish/README.md
@@ -1,1 +1,4 @@
+# Varnish Pack
 
+This pack uses the varnishstat command to return statistics. You may need to add the user 'dataloop' to the varnish group
+in /etc/groups and then `service dataloop-agent restart` for this plugin to run.
--- varnish/plugins/varnish.py
+++ varnish/plugins/varnish.py
@@ -3,18 +3,21 @@ import subprocess
 import sys
 import time
 
-command = "/usr/bin/varnishstat -1 | awk '{print $1,$2}'"
-
 
 def get_varnish_metrics():
     try:
-        status = subprocess.check_output(command, shell=True)
+        ps = subprocess.Popen(('/usr/bin/varnishstat', '-1'), stdout=subprocess.PIPE)
+        data = subprocess.check_output(('awk', '{print $1,$2}'), stdin=ps.stdout)
+        ps.wait()
+        if ps.returncode != 0:
+            print "Plugin Failed!"
+            sys.exit(2)
         m = {}
-        for line in status.splitlines():
-            name = line.split()[0].lower().replace('(', '_').replace(')', '_').replace(',,', '_')
-            m[name] = line.split()[1]
+        for line in data.splitlines():
+            if len(line.split()) > 0:
+                name = line.split()[0].lower().replace('(', '_').replace(')', '_').replace(',,', '_')
+                m[name] = line.split()[1]
         return m
-
     except Exception, e:
         print "Plugin Failed! %s" % e
         sys.exit(2)
